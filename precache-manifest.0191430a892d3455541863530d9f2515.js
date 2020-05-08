c19d748be10211d8ae3c82d5c51fb23f7c57188f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-08 12:45:55 +0000

    Deploy anilcs240/travis-demo to github.com/anilcs240/travis-demo.git:gh-pages

---
+++ precache-manifest.0191430a892d3455541863530d9f2515.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1564730c772a6625a54ac45478d01c4f",
+    "url": "/travis-demo/index.html"
+  },
+  {
+    "revision": "9b3a7ccd497c9f8c745d",
+    "url": "/travis-demo/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e2bd8e39ca37f3da6788",
+    "url": "/travis-demo/static/js/2.3d809811.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/travis-demo/static/js/2.3d809811.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b3a7ccd497c9f8c745d",
+    "url": "/travis-demo/static/js/main.7dd27abf.chunk.js"
+  },
+  {
+    "revision": "b0e316f95c52a9ceef92",
+    "url": "/travis-demo/static/js/runtime-main.eaf52929.js"
+  }
+]);
